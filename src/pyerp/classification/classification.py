22d61b6d78295b7bc45506295d6bae6c3bc1d24d
Simon Kojima <simon.kojima@outlook.com>
2024-02-10 20:37:19 +0900

    update vectorizer

--- src/pyerp/classification/classification.py
+++ src/pyerp/classification/classification.py
@@ -67,11 +67,23 @@ def classify_binary(epochs,
 class EpochsVectorizer():
     def __init__(self,
                  ivals,
-                 channel_prime = True):
+                 channel_prime = True,
+                 type = 'mne',
+                 tmin = None,
+                 tmax = None,
+                 include_tmax = True,
+                 fs = None):
         self.ivals = ivals
         self.channel_prime = channel_prime
+        self.type = type
+        self.tmin = tmin
+        self.tmax = tmax
+        self.include_tmax = include_tmax
+        self.fs = fs
         
         """
+        
+        type: 'mne', 'ndarray'
         
         channel_prime:
         if True,  vec = [ch1_t1, ch2_t1, ch3_t1, ch1_t2, ch2_t2, ch3_t2, ...]
@@ -86,10 +98,24 @@ class EpochsVectorizer():
 
     def transform(self, X):
         
-        tmin = X.times[0]
-        tmax = X.times[-1]
+        if self.type == 'ndarray':
+            tmin = self.tmin
+            tmax = self.tmax
+            fs = self.fs
+            data = X
+            if self.include_tmax:
+                times = np.linspace(start=tmin, stop=tmax, num=int((tmax-tmin)*fs)+1)
+            else:
+                times = np.linspace(start=tmin, stop=tmax, num=int((tmax-tmin)*fs))
+            if data.shape[2] != times.size:
+                raise ValueError("number of time samples is invalid.")
+        elif self.type == 'mne':
+            tmin = X.times[0]
+            tmax = X.times[-1]
+            data = X.get_data(copy = True)
+        else:
+            raise ValueError("Unknown type: %s"%str(self.type))
         
-        data = X.get_data(copy = True)
         
         if np.min(np.array(self.ivals).flatten()) < tmin:
             raise ValueError("minimum time value of ivals belows tmin of epochs.")
@@ -100,7 +126,13 @@ class EpochsVectorizer():
         vec = np.zeros((data.shape[0], data.shape[1], len(self.ivals)))
 
         for m, ival in enumerate(self.ivals):
-            idx = X.time_as_index(ival)
+            if self.type == 'ndarray':
+                idx = list()
+                for t in ival:
+                    I = np.argmin(np.absolute(times - t))
+                    idx.append(I)
+            elif self.type == 'mne':
+                idx = X.time_as_index(ival)
             idx = list(range(idx[0], idx[1]+1))
             vec[:, :, m] = np.mean(data[:, :, idx], axis=2)
 
